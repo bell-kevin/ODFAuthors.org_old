76f932891d2fe89e3763eadf83e1bade5a2b659e
andreasma <maand@gmx.de>
2017-01-25 20:53:12 +0100

    Small fix in the comments

--- src/odfauthors.policy/setup.py
+++ src/odfauthors.policy/setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Installer for the tdf.exttempsitepolicy package."""
+"""Installer for the odfauthorssitepolicy package."""
 
 from setuptools import find_packages
 from setuptools import setup
